7db15fa9c25ee676b0444afa4e8095d32a6e798a
prestongramberg <knighttraintint@gmail.com>
2024-02-07 21:28:47 -0600

    Profile SPA: Single Page Application

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,14 @@
 import './bootstrap';
 import Search from './live-search.js';
 import Chat from "./chat";
+import Profile from "./profile";
 
 if (document.querySelector(".header-search-icon")) {
     new Search();
+}
+
+if (document.querySelector(".profile-nav")) {
+    new Profile();
 }
 
 if (document.querySelector(".header-chat-icon")) {
